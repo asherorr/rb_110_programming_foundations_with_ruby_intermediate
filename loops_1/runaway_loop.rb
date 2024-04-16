7ca0a936dd3bcee2acb1c02997245c67d6391c0a
asherorr <asher@mailfence.com>
2024-04-16 16:58:12 +0000

    This commit will add the loops_1 folder and runaway_loop.rb file.

---
+++ loops_1/runaway_loop.rb
@@ -0,0 +1,12 @@
+#Modify the code so the loop stops after the first iteration.
+
+# loop do
+#   puts 'Just keep printing...'
+# end
+
+#modified:
+
+loop do
+  puts 'Just keep printing...'
+  break
+end
