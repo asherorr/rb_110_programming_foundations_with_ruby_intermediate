535b80feaa3cacf120e498ac0d4f6d5042dcdf77
asherorr <asher@mailfence.com>
2024-05-12 20:03:04 +0000

    This commit will add the code I wrote for the display_board, player_places_piece, empty_squares, and initialize_board methods for tic_tac_toe.rb.

--- lesson_3_slightly_larger_programs/tic_tac_toe.rb
+++ lesson_3_slightly_larger_programs/tic_tac_toe.rb
@@ -1,3 +1,9 @@
+require 'pry'
+
+INITIAL_MARKER = " "
+PLAYER_MARKER = "X"
+COMPUTER_MARKER = "O"
+
 def prompt(msg)
   puts "-> #{msg}"
 end
@@ -20,12 +26,12 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = ' '}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] == " "}
+  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
 end
 
 def player_places_piece(brd)
@@ -33,20 +39,19 @@ def player_places_piece(brd)
   loop do
     prompt "Choose a square (#{empty_squares(brd).join(", ")}):"
     square = gets.chomp.to_i
-    if empty_squares(brd).include?(square)
-      break
-    else
-      prompt "Sorry, that's not a valid choice."
-    end
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
   end
-
-  brd[square] = 'X'
+  binding.pry
+  brd[square] = PLAYER_MARKER
 end
 
 
 board = initialize_board
 display_board(board)
 
+
 player_places_piece(board)
+comouter_places_piece(board)
 puts board.inspect
 display_board(board)
