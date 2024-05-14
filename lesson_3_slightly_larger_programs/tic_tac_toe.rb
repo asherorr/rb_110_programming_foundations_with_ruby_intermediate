562ed9533c79f6e8b8dd429c20f1ad730a53fb4a
asherorr <asher@mailfence.com>
2024-05-14 18:11:59 +0000

    This commit will add a feature to make the computer defensive minded - so that if there's an immediate threat, then it will defend the 3rd square.

--- lesson_3_slightly_larger_programs/tic_tac_toe.rb
+++ lesson_3_slightly_larger_programs/tic_tac_toe.rb
@@ -3,6 +3,9 @@ require 'pry'
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
+WINNING_LINES = [[1, 2, 3], [4, 5, 6],
+                   [7, 8, 9]] + [[1, 4, 7], [2, 5, 8],
+                                 [3, 6, 9]] + [[1, 5, 9], [3, 5, 7]]
 
 def prompt(msg)
   puts "-> #{msg}"
@@ -56,8 +59,26 @@ def player_places_piece(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def find_at_risk_square(line, board)
+  if board.values_at(*line).count(PLAYER_MARKER) == 2
+    board.select{|k, v| line.include?(k) && v == INITIAL_MARKER}.keys.first
+  else
+    nil
+  end
+end
+
+
 def computer_places_piece(brd)
-  square = empty_squares(brd).sample
+  square = nil
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd)
+    break if square
+  end
+  
+  if !square
+    square = empty_squares(brd).sample
+  end
+  
   brd[square] = COMPUTER_MARKER
 end
 
@@ -70,11 +91,7 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6],
-                   [7, 8, 9]] + [[1, 4, 7], [2, 5, 8],
-                                 [3, 6, 9]] + [[1, 5, 9], [3, 5, 7]]
-
-  winning_lines.each do |line|
+  WINNING_LINES.each do |line|
     if brd[line[0]] == PLAYER_MARKER && brd[line[1]] == PLAYER_MARKER && brd[line[2]] == PLAYER_MARKER
       return 'Player'
     elsif brd[line[0]] == COMPUTER_MARKER && brd[line[1]] == COMPUTER_MARKER && brd[line[2]] == COMPUTER_MARKER
